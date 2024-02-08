f87c2053c0ed49b7540f06efe4ece61fa03606e5
ashwath462 <ashwatharora462@gmail.com>
2024-02-08 13:16:05 +0530

    added routing

--- src/lib/components/common/Border.svelte
+++ src/lib/components/common/Border.svelte
@@ -1,5 +1,4 @@
-<script>
-
+<script lang="ts">
 </script>
 
 <div class="border-b-2 border-gray-300"></div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
     import { goto } from "$app/navigation";
     import {onMount} from 'svelte';
-
+    import Loader from "$lib/components/Common/Loader.svelte";
     onMount(()=>{
         goto('FlightsPage');
     })
 </script>
+
+<Loader/>
