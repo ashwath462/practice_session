bb4afe690c9d35de9d46e3d5d1224998647b4cf0
ashwath462 <ashwatharora462@gmail.com>
2024-02-09 14:17:00 +0530

    added calendar

--- src/lib/components/LandingPage/DateSelector.svelte
+++ src/lib/components/LandingPage/DateSelector.svelte
@@ -2,41 +2,11 @@
 	import AddSign from '$lib/images/AddSign.svelte';
     import dayjs from 'dayjs';
     import { addYears } from 'date-fns';
-	import { getDate } from '$lib/utils/utils';
     import { Datepicker } from 'svelte-calendar';
-	const { day, date, month } = getDate();
+	import { calendarStyles } from '$lib/utils/constants';
     const endDate = addYears(new Date(), 1);
     let store:any;
-    const calendarStyles = {
-        "calendar": {
-            "width": "700px",
-            "maxWidth": "90vw",
-            "legend": {
-            "height": "45px"
-            },
-            "shadow": "0px 10px 26px rgba(0, 0, 0, 0.25)",
-            "colors": {
-            "text": {
-                "primary": "#333",
-                "highlight": "#fff"
-            },
-            "background": {
-                "primary": "#fff",
-                "highlight": "#00b5ff",
-                "hover": "#eee"
-            },
-            "border": "#eee"
-            },
-            "font": {
-            "regular": "1.5em",
-            "large": "37em"
-            },
-            "grid": {
-            "disabledOpacity": ".35",
-            "outsiderOpacity": ".6"
-            }
-        }
-    }
+    
 </script>
 
 <div class="card bg-base-100 px-2 m-4 rounded-sm">
